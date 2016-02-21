127137de728ae78e26aa3fa8626195b29b1522e2
eli <eli@localhost>
2016-02-20 19:46:19 -0800

    keyboard control

--- robot.js
+++ robot.js
@@ -4,3 +4,43 @@ function move(dir) {
 	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
 	xmlhttp.send('dir=' + dir);
 }
+
+function say(dir) {
+	xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("POST","/say",true);
+	xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	xmlhttp.send('dir=' + dir);
+}
+
+function keypress(event) {
+	if (event.keyCode == 13) {
+		say(document.getElementById("tts").value);
+		document.getElementById("tts").value = "";
+	}
+}
+
+document.onkeypress = function(event) {
+	if (event.target.nodeName.toUpperCase() == 'INPUT'
+	 && event.target.type.toUpperCase() == 'TEXT')
+		return;
+
+	switch (event.keyCode & ~32) {
+	case 87:
+		move('f');
+		break;
+	case 68:
+		move('r');
+		break;
+	case 65:
+		move('l');
+		break;
+	case 82:
+		move('u');
+		break;
+	case 70:
+		move('d');
+		break;
+	default:
+		break;
+	}
+}
